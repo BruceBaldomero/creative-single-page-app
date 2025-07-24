a308a6b3c4f6426de42ffd61e4957beb431ac5ac
BruceBaldomero <brucebaldomero@gmail.com>
2025-07-24 18:33:54 +0100

    Updates

---
+++ precache-manifest.0032667bb6cf0c666de5fe22e4351fb0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9b1e6bcd79bfa08e54547f62cec2f6b",
+    "url": "/creative-single-page-app/index.html"
+  },
+  {
+    "revision": "a4dcfad4ab8b7c4f0028",
+    "url": "/creative-single-page-app/static/css/main.d16dcaf7.chunk.css"
+  },
+  {
+    "revision": "078d0c4249ec834a93d2",
+    "url": "/creative-single-page-app/static/js/2.b9b275f8.chunk.js"
+  },
+  {
+    "revision": "ad951958ed7766763ed3e6c72917393c",
+    "url": "/creative-single-page-app/static/js/2.b9b275f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4dcfad4ab8b7c4f0028",
+    "url": "/creative-single-page-app/static/js/main.b1cdb641.chunk.js"
+  },
+  {
+    "revision": "475c004407547f1251f3",
+    "url": "/creative-single-page-app/static/js/runtime-main.d38899dc.js"
+  }
+]);
